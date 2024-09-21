c4d1c307b834892bfb4adfd96305baab76f6c7a3
Paulo Guimarães <elmuchacho@outlook.pt>
2024-09-21 23:53:44 +0100

    teste

--- PythonExercises/ex093.py
+++ PythonExercises/ex093.py
@@ -1,3 +1,4 @@
 '''
 093. Cria um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e o número de partidas que ele jogou. Em seguida, vai ler a quantidade de golos feitos em cada partida. No final, todas essas informações serão guardadas num dicionário, incluindo o total de golos marcados durante o campeonato.
 '''
+dis
